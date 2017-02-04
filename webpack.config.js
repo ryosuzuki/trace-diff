c874b13254c677f951e1b7dafee56a0f6a526535
Ryo Suzuki <ryosuzk@gmail.com>
2017-02-04 12:09:19 -0700

    Webpack-dev-server with hot reload

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    bundle: './src/index.js',
+    html: './index.html'
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['react', 'es2015', 'stage-3']
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    compress: true,
+    hot: true,
+    inline: true,
+    watchContentBase: true,
+    port: 8080
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+
+}

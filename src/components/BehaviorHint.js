4c78eba55a11073f3a514c0cdaad04da45b8140a
Ryo Suzuki <ryosuzk@gmail.com>
2017-02-06 17:02:40 -0700

    Add call label

--- src/components/BehaviorHint.js
+++ src/components/BehaviorHint.js
@@ -7,7 +7,7 @@ class BehaviorHint extends Component {
   constructor() {
     super()
     this.state = {
-      code: '',
+      code: '\n',
       step: 0
     }
     window.behaviorHint = this
@@ -19,21 +19,24 @@ class BehaviorHint extends Component {
       url: '/data/behavior-hint-1.py',
     })
     .then((res) => {
-      this.setState({ code: res })
       let origin = []
       for (let line of res.split('\n')) {
         origin.push(line)
       }
-      this.setState({ code: res, origin: origin })
+      this.setState({ code1: res, origin: origin })
     })
   }
 
   showHint() {
-    this.cm = this.refs.editor.getCodeMirror()
+    this.cm = this.refs.editor1.getCodeMirror()
     let msg = document.createElement('div')
     msg.append($('.table')[0])
     // debugger
     this.cm.addLineWidget(4, msg)
+
+    let marker = document.createElement('div')
+    marker.append($('.label')[0])
+    this.cm.setGutterMarker(3, 'breakpoints', marker)
   }
 
   updateStep(value) {
@@ -45,7 +48,8 @@ class BehaviorHint extends Component {
     const options = {
       mode: 'python',
       theme: 'base16-light',
-      lineNumbers: true
+      lineNumbers: true,
+      gutters: ['CodeMirror-linenumbers', 'breakpoints']
     }
 
     return (
@@ -53,8 +57,8 @@ class BehaviorHint extends Component {
         <h1>Behavior Hint</h1>
         <p className="ui text">{ description }</p>
         <button className="ui basic button" onClick={ this.showHint.bind(this) }>Show Hint</button>
-        <CodeMirror value={ this.state.code }
-                    ref="editor"
+        <CodeMirror value={ this.state.code1 }
+                    ref="editor1"
                     options={ options }
         />
         <Slider
@@ -65,6 +69,9 @@ class BehaviorHint extends Component {
           onChange={ this.updateStep.bind(this) }
         />
         <div style={{ display: 'none' }}>
+        <div className="ui blue label call-label">
+          Blue
+        </div>
         <table className="ui definition table">
           <thead>
             <tr>

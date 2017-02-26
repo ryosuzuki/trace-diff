ccfd06174fc399990aa7ce024733bdcc139533fa
Ryo Suzuki <ryosuzk@gmail.com>
2017-02-25 22:15:32 -0700

    Track call stacks for the history

--- src/components/Record.js
+++ src/components/Record.js
@@ -11,78 +11,99 @@ class Record {
   }
 
   generate(traces, type) {
-    let history = {}
-    let ticks = {}
-    let lines = {}
+    this.stacks = []
+    this.history = {}
+    this.ticks = {}
     for (let i = 0; i < traces.length; i++) {
       let trace = traces[i]
       for (let func of Object.keys(trace.locals)) {
-
         if (func === 'accumulate') {
-          let variables = trace.locals[func]
-          for (let key of Object.keys(variables)) {
-            if (key === '__return__') continue
-
-            let value = variables[key]
-            if (value === undefined) continue
-
-            if (!history[key]) history[key] = []
-            if (!lines[key]) lines[key] = []
-            if (!ticks[key]) ticks[key] = {}
-
-            let line = trace.line
-            if (trace.event === 'step_line') {
-              line = traces[i-1].line
-            }
-            let last = _.last(history[key])
-            if (last === undefined || last !== value) {
-              history[key].push(value)
-              lines[key].push(line)
-            }
-            ticks[key][i] = history[key].length
-            if (trace.event === 'step_line') {
-              ticks[key][i-1] = history[key].length
-            }
-          }
+          this.addValue(traces, i, func)
         }
-
-        if (trace.event === 'return') {
-          let value = ''
-          let args = {}
-          for (let key of Object.keys(trace.locals[func])) {
-            if (key === '__return__') {
-              value = trace.locals[func][key]
-            } else {
-              args[key] = trace.locals[func][key]
-            }
-          }
-          let key = `${func}(${Object.values(args).join(', ')})`
-          if (func === 'accumulate') {
-            key = func
-            key += '('
-            key += args['combiner']
-            key += ', '
-            key += args['base']
-            key += ', '
-            key += args['n']
-            key += ', '
-            key += args['term']
-            key += ')'
-          }
-          history[key] = [value]
-        }
-
       }
+      if (trace.event === 'call') this.addCall(trace)
+      if (trace.event === 'return') this.addReturn(trace)
     }
 
     if (type === 'before') {
-      this.beforeHistory = history
-      this.beforeTicks = ticks
+      this.beforeHistory = this.history
+      this.beforeTicks = this.ticks
     } else if (type === 'after') {
-      this.afterHistory = history
-      this.afterTicks = ticks
+      this.afterHistory = this.history
+      this.afterTicks = this.ticks
     }
-    return { history: history, lines: lines, ticks: ticks }
+    return { history: this.history, ticks: this.ticks }
+  }
+
+  addCall(trace) {
+    let func = trace.func_name
+    if (func === "<module>") return
+    let args = {}
+    for (let key of Object.keys(trace.locals[func])) {
+      args[key] = trace.locals[func][key]
+    }
+    let key = this.getKey(func, args)
+    let node = {
+      type: 'call',
+      calls: [],
+      value: ''
+    }
+    this.history[key] = node
+
+    let last = _.last(this.stacks)
+    if (last) this.history[last].calls.push(key)
+    this.stacks.push(key)
+  }
+
+  addReturn(trace) {
+    let func = trace.func_name
+    if (func === "<module>") return
+    let args = {}
+    for (let key of Object.keys(trace.locals[func])) {
+      if (key === '__return__') continue
+      args[key] = trace.locals[func][key]
+    }
+    let key = this.getKey(func, args)
+    let value = trace.locals[func]['__return__']
+    this.history[key]['value'] = value
+
+    this.stacks.pop(key)
+  }
+
+  getKey(func, args) {
+    if (func === 'accumulate') {
+      const ks = ['combiner', 'base', 'n', 'term']
+      return `${func}(${ks.map(k => args[k]).join(', ')})`
+    } else {
+      return `${func}(${Object.values(args).join(', ')})`
+    }
+  }
+
+  addValue(traces, i, func) {
+    let trace = traces[i]
+    let variables = trace.locals[func]
+    for (let key of Object.keys(variables)) {
+      let value = variables[key]
+      if (key === '__return__') continue
+      if (value === undefined) continue
+
+      let node = {
+        type: 'name',
+        values: [value],
+        value: value
+      }
+      if (!this.history[key]) this.history[key] = node
+      if (!this.ticks[key]) this.ticks[key] = {}
+      if (this.history[key]['value'] !== value) {
+        this.history[key]['value'] = value
+        this.history[key]['values'].push(value)
+      }
+      this.ticks[key][i] = this.history[key].length
+      if (trace.event === 'step_line') {
+        this.ticks[key][i-1] = this.history[key].length
+      }
+    }
+
   }
 
   check() {
@@ -106,3 +127,35 @@ class Record {
 }
 
 export default Record
+
+
+/*
+
+  addValue(traces, i, func) {
+    let trace = traces[i]
+    let variables = trace.locals[func]
+    for (let key of Object.keys(variables)) {
+      let value = variables[key]
+      if (key === '__return__') continue
+      if (value === undefined) continue
+
+      if (!this.history[key]) this.history[key] = []
+      if (!this.lines[key]) this.lines[key] = []
+      if (!this.ticks[key]) this.ticks[key] = {}
+
+      let line = trace.line
+      if (trace.event === 'step_line') {
+        line = traces[i-1].line
+      }
+      let last = _.last(this.history[key])
+      if (last === undefined || last !== value) {
+        this.history[key].push(value)
+        this.lines[key].push(line)
+      }
+      this.ticks[key][i] = this.history[key].length
+      if (trace.event === 'step_line') {
+        this.ticks[key][i-1] = this.history[key].length
+      }
+    }
+  }
+ */
